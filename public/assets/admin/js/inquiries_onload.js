64ed2e3aab6744a1de690de3fbfe40e8d1e26bcc
pensivetraveler <pensivetraveler@gmail.com>
2025-04-12 15:56:13 +0900

    feat: asset 초기화

--- public/assets/admin/js/inquiries_onload.js
+++
@@ -1,3 +0,0 @@
-function renderColumnReplyYn(data, type, full, meta, column) {
-	return data?'응답 완료':'응답 전';
-}
--- public/assets/admin/js/inquiries_preset.js
+++
@@ -1,5 +0,0 @@
-appPlugins.list.datatable = {
-	rowCallback : function(row, data, displayNum, displayIndex, dataIndex) {
-		if(data.reply_yn) $(row).css('background-color', '#cbcbcb');
-	}
-}
