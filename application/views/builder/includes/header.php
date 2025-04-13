08bc19dfb1a5046decd71e8d8f1360c94b003ee6
pensivetraveler <pensivetraveler@gmail.com>
2025-04-14 05:04:04 +0900

    feat: core updated

--- application/views/builder/includes/header.php
+++ application/views/builder/includes/header.php
@@ -6,13 +6,14 @@
 				<aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
 					<div class="app-brand demo">
 						<!-- Logo -->
-						<a href="/builder/dashboard?layout=side-menu" class="app-brand-link">
-									<span class="app-brand-logo demo">
-										<img src="https://playtalktalk.thefit.io/public/assets/builder/img/eduprime_logo.png" alt="">
+						<a href="/<?=$platformName?>/dashboard?layout=side-menu" class="app-brand-link">
+							<?php if(file_exists(PLATFORM_ASSET_IMG_PATH.'logo.png')): ?>
+								<span class="app-brand-logo demo">
+										<img src="<?=base_url(PLATFORM_ASSET_IMG_URI.'logo.png')?>" alt="logo" class="w-px-100">
 									</span>
-							<!--
-							<span class="app-brand-text demo menu-text fw-semibold ms-2 text-primary">(주)에듀프라임</span>
-							-->
+							<?php else: ?>
+								<span class="app-brand-text demo menu-text fw-semibold ms-2 text-primary"><?=APP_NAME_KR?></span>
+							<?php endif; ?>
 						</a>
 						<!-- / Logo -->
 
