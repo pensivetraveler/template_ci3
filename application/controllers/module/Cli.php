8a40a50b5af1d5bee7333a328647e05ea7656001
pensivetraveler <pensivetraveler@gmail.com>
2025-04-16 02:13:16 +0900

    feat: controller related cli

---
+++ application/controllers/module/Cli.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+require_once __DIR__.'/Common.php';
+
+class Cli extends Common
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
--- application/controllers/module/Common.php
+++ application/controllers/module/Common.php
@@ -1,9 +1,13 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-class Common extends MY_Controller_WEB
+class Common extends MY_Controller
 {
     function __construct()
     {
         parent::__construct();
+
+        if (php_sapi_name() !== 'cli') {
+            show_error('Direct access is not allowed');
+        }
     }
 }
