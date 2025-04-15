0275c76b0e0180c169a02bdad3598b0464ef7c11
pensivetraveler <pensivetraveler@gmail.com>
2025-04-16 04:13:44 +0900

    refactor: 혹시나 대비하기 위해, routing에서 중요한 Function 들 추출

--- application/core/MY_Router.php
+++ application/core/MY_Router.php
@@ -5,7 +5,6 @@ class MY_Router extends CI_Router
 {
     protected function _set_default_controller()
     {
-
         if (empty($this->default_controller)) {
 
             show_error('Unable to determine what should be displayed. A default route has not been specified in the routing file.');
@@ -62,4 +61,68 @@ class MY_Router extends CI_Router
 
         log_message('debug', 'No URI present. Default controller set.');
     }
+
+//    protected function _validate_request($segments)
+//    {
+//        $c = count($segments);
+//        $directory_override = $this->directory !== '';
+//
+//        // Loop through our segments and return as soon as a controller
+//        // is found or when such a directory doesn't exist
+//        while ($c-- > 0)
+//        {
+//            $test = $this->directory
+//                .ucfirst($this->translate_uri_dashes === TRUE ? str_replace('-', '_', $segments[0]) : $segments[0]);
+//
+//            if ( ! file_exists(APPPATH.'controllers/'.$test.'.php')
+//                && $directory_override === FALSE
+//                && is_dir(APPPATH.'controllers/'.$this->directory.$segments[0])
+//            )
+//            {
+//                $this->set_directory(array_shift($segments), TRUE);
+//                continue;
+//            }
+//
+//            return $segments;
+//        }
+//
+//        // This means that all segments were actually directories
+//        return $segments;
+//    }
+//
+//    protected function _set_request($segments = array())
+//    {
+//        $segments = $this->_validate_request($segments);
+//
+//        // If we don't have any segments left - try the default controller;
+//        // WARNING: Directories get shifted out of the segments array!
+//        if (empty($segments))
+//        {
+//            $this->_set_default_controller();
+//            return;
+//        }
+//
+//        if ($this->translate_uri_dashes === TRUE)
+//        {
+//            $segments[0] = str_replace('-', '_', $segments[0]);
+//            if (isset($segments[1]))
+//            {
+//                $segments[1] = str_replace('-', '_', $segments[1]);
+//            }
+//        }
+//
+//        $this->set_class($segments[0]);
+//        if (isset($segments[1]))
+//        {
+//            $this->set_method($segments[1]);
+//        }
+//        else
+//        {
+//            $segments[1] = 'index';
+//        }
+//
+//        array_unshift($segments, NULL);
+//        unset($segments[0]);
+//        $this->uri->rsegments = $segments;
+//    }
 }
