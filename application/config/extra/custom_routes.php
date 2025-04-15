9378b8ca1e6dd5e107b63d1edf7d892eec194a57
pensivetraveler <pensivetraveler@gmail.com>
2025-04-16 02:12:46 +0900

    feat: api route rule add

--- application/config/extra/custom_routes.php
+++ application/config/extra/custom_routes.php
@@ -17,13 +17,32 @@ foreach ($route['except_folders'] as $name) {
 			show_404();
 		};
 	}else{
-		$route[$name] = $name.DIRECTORY_SEPARATOR.$route['default_controller'];
-		// 컨트롤러/메서드/파라미터 (숫자) 지원
-		$route["$name/(:any)/(:any)/(:num)"] = "$name/$1/$2/$3";
-		$route["$name/(:any)/(:num)"] = "$name/$1/index/$2";
-		// 컨트롤러/메서드 구조 지원
-		$route["$name/(:any)/(:any)"] = "$name/$1/$2";
-		$route["$name/(:any)"] = "$name/$1";
+        if(!in_array($name, $route['api_folders'])){
+            $route[$name] = $name.DIRECTORY_SEPARATOR.$route['default_controller'];
+            // 컨트롤러/메서드/파라미터 (숫자) 지원
+            $route["$name/(:any)/(:any)/(:num)"] = "$name/$1/$2/$3";
+            $route["$name/(:any)/(:num)"] = "$name/$1/index/$2";
+            // 컨트롤러/메서드 구조 지원
+            $route["$name/(:any)/(:any)"] = "$name/$1/$2";
+            $route["$name/(:any)"] = "$name/$1";
+
+            foreach ($route['api_folders'] as $api) {
+                // 컨트롤러/메서드/파라미터 (숫자) 지원
+                $route["$name/$api/(:any)/(:any)/(:num)"] = "api/$1/$2/$3";
+                $route["$name/$api/(:any)/(:num)"] = "api/$1/index/$2";
+                // 컨트롤러/메서드 구조 지원
+                $route["$name/$api/(:any)/(:any)"] = "api/$1/$2";
+                $route["$name/$api/(:any)"] = "api/$1";
+            }
+        }else{
+            $route[$name] = $name.DIRECTORY_SEPARATOR.$route['default_controller'];
+            // 컨트롤러/메서드/파라미터 (숫자) 지원
+            $route["$name/(:any)/(:any)/(:num)"] = "$name/$1/$2/$3";
+            $route["$name/(:any)/(:num)"] = "$name/$1/index/$2";
+            // 컨트롤러/메서드 구조 지원
+            $route["$name/(:any)/(:any)"] = "$name/$1/$2";
+            $route["$name/(:any)"] = "$name/$1";
+        }
 	}
 }
 
