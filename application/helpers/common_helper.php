9475a75744d69a127b6194da42e99f59d2583f6e
pensivetraveler <pensivetraveler@gmail.com>
2025-04-17 23:05:59 +0900

    debug: is_empty에 대해서, 재귀형태로 오류발생시키는 오류 정정

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -14,33 +14,34 @@ if ( ! function_exists('is_empty'))
 {
 	function is_empty($data, $key = ''): bool
 	{
-		if(is_null($data)) return true;
-		if(!trim($key)) {
-			switch(gettype($data)) {
-				case 'boolean' :
-					return !$data;
-				case 'object' :
-					$data = get_object_vars($data);
-				case 'array' :
-					if(empty($data)) return true;
-					break;
-				default :
-					// 0 을 구분하기 위해 strlen 조건 추가
-					if(empty($data) && strlen((string)$data) === 0) return true;
-			}
-			return false;
-		}else{
-			switch (gettype($data)) {
-				case 'object' :
-					if(!property_exists($data, $key)) return true;
-					return is_empty($data->{$key});
-				case 'array' :
-					if(!array_key_exists($key, $data)) return true;
-					return is_empty($data[$key]);
-				default :
-					return true;
-			}
-		}
+        if(is_null($data)) return true;
+        if(!isset($data[$key])) return true;
+        if(!trim($key)) {
+            switch(gettype($data)) {
+                case 'boolean' :
+                    return !$data;
+                case 'object' :
+                    $data = get_object_vars($data);
+                case 'array' :
+                    if(empty($data)) return true;
+                    break;
+                default :
+                    // 0 을 구분하기 위해 strlen 조건 추가
+                    if(empty($data) && strlen((string)$data) === 0) return true;
+            }
+            return false;
+        }else{
+            switch (gettype($data)) {
+                case 'object' :
+                    if(!property_exists($data, $key)) return true;
+                    return empty($data->{$key});
+                case 'array' :
+                    if(!array_key_exists($key, $data)) return true;
+                    return empty($data[$key]);
+                default :
+                    return true;
+            }
+        }
 	}
 }
 
