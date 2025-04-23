06ec601270fb8687ea110362ddaf34a26291bf10
pensivetraveler <pensivetraveler@gmail.com>
2025-04-24 06:22:04 +0900

    feat: view_base, view_grid added

--- application/views/builder/layout/view_base.php
+++ application/views/builder/layout/view_base.php
@@ -1,10 +1,90 @@
 <div class="card">
 	<div class="card-header">
-		<div class="mb-4 d-flex align-items-center justify-content-between">
-			<h5 id="subject" class="mb-0 w-100 view-data"><span></span></h5>
+		<div class="d-flex align-items-center justify-content-between">
+			<h5 class="mb-0 w-100"><span></span></h5>
+			<div class="btn-dropdown-wrapper">
+				<div class="dropdown">
+					<button
+							class="btn btn-text-secondary rounded-pill text-muted border-0 p-1"
+							type="button"
+							id="actionBtns"
+							data-bs-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false">
+						<i class="ri-more-2-line ri-20px"></i>
+					</button>
+					<div class="dropdown-menu dropdown-menu-end" aria-labelledby="actionBtns">
+						<?php
+							if(count($buttons)) :
+								echo '<hr class="my-2">';
+								foreach ($buttons as $button):
+							?>
+							<a class="dropdown-item text-center btn-view-<?=$button?>" href="javascript:void(0);"><?=lang(ucfirst($button))?></a>
+							<?php
+								endforeach;
+							endif;
+						?>
+						<?php foreach ($actions as $action): ?>
+							<a class="dropdown-item text-center btn-view-<?=$action?>" href="javascript:void(0);"><?=lang(ucfirst($action))?></a>
+						<?php endforeach; ?>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 	<div class="card-body">
-
+		<?php
+			echo form_open_multipart('', [
+				'id' => 'formRecord',
+				'class' => "add-new-record needs-validation view-type-{$viewType}",
+				'onsubmit' => 'return false',
+			], [
+				'_mode' => $this->router->method,
+				'_event' => '',
+			]);
+			foreach ($viewData['hiddens'] as $item) :
+				echo form_input(
+					[
+						'type' => $item['type'],
+						'name' => $item['field'],
+						'id' => $item['id'],
+					],
+					'',
+					$item['attributes'],
+				);
+			endforeach;
+			foreach ($viewData['fields'] as $item) :
+		?>
+		<div class="row mb-4" data-field-name="<?=$item['field']?>">
+			<?=form_label(lang($item['label']), $item['id'], ['class' => 'col-sm-2 col-form-label fs-6 text-primary fw-bolder py-4'])?>
+			<div class="col-sm-10">
+				<div class="w-100 d-flex align-items-center border-bottom text-sm-left py-4">
+					<span id="<?=$item['id']?>"></span>
+				</div>
+				<?php if(!empty($item['help_block'])) echo get_help_block_html($item['help_block']); ?>
+			</div>
+		</div>
+		<?php
+			endforeach;
+		?>
+		<div class="row mt-6">
+			<div class="col-sm-6 text-start">
+				<?php foreach ($buttons as $item): ?>
+				<button type="button" class="btn btn-outline-dark waves-effect"><?=lang($item['text'])?></button>
+				<?php endforeach; ?>
+			</div>
+			<div class="col-sm-6 text-end">
+				<?php if(in_array('list', $actions)): ?>
+				<button type="button" class="btn btn-outline-dark waves-effect btn-view-list"><?=lang('List')?></button>
+				<?php endif; ?>
+				<?php if(in_array('edit', $actions)): ?>
+				<button type="button" class="btn btn-primary waves-effect btn-view-edit"><?=lang('Edit')?></button>
+				<?php endif; ?>
+				<?php if(in_array('delete', $actions)): ?>
+				<button type="button" class="btn btn-outline-danger waves-effect btn-view-delete"><?=lang('Delete')?></button>
+				<?php endif; ?>
+			</div>
+		</div>
+		<?=form_close();?>
 	</div>
 </div>
---
+++ application/views/builder/layout/view_grid.php
@@ -0,0 +1,107 @@
+<div class="card">
+	<div class="card-header">
+		<div class="d-flex align-items-center justify-content-between">
+			<h5 class="mb-0 w-100"><span></span></h5>
+			<div class="btn-dropdown-wrapper">
+				<div class="dropdown">
+					<button
+							class="btn btn-text-secondary rounded-pill text-muted border-0 p-1"
+							type="button"
+							id="actionBtns"
+							data-bs-toggle="dropdown"
+							aria-haspopup="true"
+							aria-expanded="false">
+						<i class="ri-more-2-line ri-20px"></i>
+					</button>
+					<div class="dropdown-menu dropdown-menu-end" aria-labelledby="actionBtns">
+						<?php
+							if(count($buttons)) :
+								echo '<hr class="my-2">';
+								foreach ($buttons as $button):
+						?>
+						<a class="dropdown-item text-center btn-view-<?=$button?>" href="javascript:void(0);"><?=lang(ucfirst($button))?></a>
+						<?php
+								endforeach;
+							endif;
+						?>
+						<?php foreach ($actions as $action): ?>
+						<a class="dropdown-item text-center btn-view-<?=$action?>" href="javascript:void(0);"><?=lang(ucfirst($action))?></a>
+						<?php endforeach; ?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="card-body">
+        <?php
+            echo form_open_multipart('', [
+                'id' => 'formRecord',
+                'class' => "add-new-record needs-validation view-type-{$viewType}",
+                'onsubmit' => 'return false',
+            ], [
+                '_mode' => $this->router->method,
+                '_event' => '',
+            ]);
+
+            foreach ($viewData['hiddens'] as $item) :
+                echo form_input(
+                    [
+                        'type' => $item['type'],
+                        'name' => $item['field'],
+                        'id' => $item['id'],
+                    ],
+                    '',
+                    $item['attributes'],
+                );
+            endforeach;
+
+            $list = [];
+            $cols = 0;
+            $rows = 0;
+            foreach (array_column($viewData['fields'], 'colspan') as $key=>$colspan){
+                if($cols + $colspan > 12) {
+                    $cols = 0;
+                    $rows++;
+                }
+                $list[$rows][] = $viewData['fields'][$key];
+                $cols += $colspan;
+            }
+            foreach ($list as $row) :
+        ?>
+            <div class="row">
+                <?php foreach ($row as $item): ?>
+                <div class="col-md-<?=$item['colspan']?> mb-2" data-field-name="<?=$item['field']?>">
+					<div class="mb-3 position-relative">
+						<?=form_label(lang($item['label']), $item['id'], ['class' => 'd-block col-form-label fs-6 text-primary py-0 mb-2 fw-bolder'])?>
+						<p class="form-control border-0 border-bottom rounded-0 mb-0">
+							<span class="d-inline-block" id="<?=$item['id']?>"></span>
+						</p>
+						<?php if(!empty($item['help_block'])) echo get_help_block_html($item['help_block']); ?>
+					</div>
+                </div>
+                <?php endforeach; ?>
+            </div>
+        <?php
+            endforeach;
+        ?>
+        <div class="row mt-6">
+            <div class="col-sm-6 text-start">
+                <?php foreach ($buttons as $item): ?>
+                    <button type="button" class="btn btn-outline-dark waves-effect"><?=lang($item['text'])?></button>
+                <?php endforeach; ?>
+            </div>
+            <div class="col-sm-6 text-end">
+                <?php if(in_array('list', $actions)): ?>
+				<button type="button" class="btn btn-outline-dark waves-effect btn-view-list"><?=lang('List')?></button>
+                <?php endif; ?>
+                <?php if(in_array('edit', $actions)): ?>
+				<button type="button" class="btn btn-primary waves-effect btn-view-edit"><?=lang('Edit')?></button>
+                <?php endif; ?>
+                <?php if(in_array('delete', $actions)): ?>
+				<button type="button" class="btn btn-outline-danger waves-effect btn-view-delete"><?=lang('Delete')?></button>
+                <?php endif; ?>
+            </div>
+        </div>
+        <?=form_close();?>
+    </div>
+</div>
