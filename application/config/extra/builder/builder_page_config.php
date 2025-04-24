3b84c044c5b76772104df9c602abcd197d404e23
pensivetraveler <pensivetraveler@gmail.com>
2025-04-24 10:47:04 +0900

    conf: base page config updated - view 기본값 false - viewProperties 에 buttons property 추가

--- application/config/extra/builder/builder_page_config.php
+++ application/config/extra/builder/builder_page_config.php
@@ -2,79 +2,80 @@
 $config['builder_page_config_loaded'] = true;
 
 $config['base_includes_config'] = [
-	'head' => true,
-	'header' => true,
-	'modalPrepend' => true,
-	'modalAppend' => true,
-	'footer' => true,
-	'tail' => true,
+    'head' => true,
+    'header' => true,
+    'modalPrepend' => true,
+    'modalAppend' => true,
+    'footer' => true,
+    'tail' => true,
 ];
 
 $config['page_base_config'] = [
-	'category' => 'page',
-	'type' => 'page',
-	'subType' => 'base',
-	'properties' => [
-		'baseMethod' => 'list',
-		'allows' => [],
-		'noIndex' => false,
-		'formExist' => false,
-		'listExist' => false,
-		'includes' => $config['base_includes_config'],
-		'allowNoLogin' => false,
-	],
-	'formProperties' => [
-		'formConfig' => '',
-		'formType' => 'page',
-	],
-	'listProperties' => [
-		'excelConfig' => '',
-		'listConfig' => '',
-		'plugin' => 'datatable',
-		'isCheckbox' => false,
-		'exports' => [
-			'print' => false,
-			'csv' => false,
-			'excel' => false,
-			'pdf' => false,
-			'copy' => false,
-		],
-		'buttons' => [
-			'add' => true,
-			'excel' => true,
-		],
-		'actions' => [
-			'edit' => true,
-			'view' => true,
-			'delete' => true,
-		],
-	],
-	'viewProperties' => [
-		'viewConfig' => '',
-		'viewType' => 'base',
-		'extraFormConfig' => '',
-		'isComments' => false,
-		'actions' => [
-			'list' => true,
-			'edit' => true,
-			'delete' => true,
-		],
-	],
-	'tabProperties' => [
-		'tabGroup' => '',
-	],
+    'category' => 'page',
+    'type' => 'page',
+    'subType' => 'base',
+    'properties' => [
+        'baseMethod' => 'list',
+        'allows' => [],
+        'noIndex' => false,
+        'formExist' => false,
+        'listExist' => false,
+        'includes' => $config['base_includes_config'],
+        'allowNoLogin' => false,
+    ],
+    'formProperties' => [
+        'formConfig' => '',
+        'formType' => 'page',
+    ],
+    'listProperties' => [
+        'listConfig' => '',
+        'excelConfig' => '',
+        'plugin' => 'datatable',
+        'isCheckbox' => false,
+        'exports' => [
+            'print' => false,
+            'csv' => false,
+            'excel' => false,
+            'pdf' => false,
+            'copy' => false,
+        ],
+        'buttons' => [
+            'add' => true,
+            'excel' => true,
+        ],
+        'actions' => [
+            'edit' => true,
+            'view' => false,
+            'delete' => true,
+        ],
+    ],
+    'viewProperties' => [
+        'viewConfig' => '',
+        'viewType' => 'base',
+        'extraFormConfig' => '',
+        'isComments' => false,
+        'buttons' => [],
+        'actions' => [
+            'list' => true,
+            'edit' => true,
+            'delete' => true,
+        ],
+    ],
+    'tabProperties' => [
+        'tabGroup' => '',
+    ],
 ];
 
 $config['modal_base_config'] = array_replace_recursive($config['page_base_config'], [
-	'category' => 'modal',
-	'properties' => [
-		'includes' => [
-			'head' => false,
-			'header' => false,
-			'modalPrepend' => false,
-			'modalAppend' => false,
-			'footer' => false,
-			'tail' => false,
-		],
-	],
+    'category' => 'modal',
+    'properties' => [
+        'includes' => [
+            'head' => false,
+            'header' => false,
+            'modalPrepend' => false,
+            'modalAppend' => false,
+            'footer' => false,
+            'tail' => false,
+        ],
+    ],
 ]);
