e16ab9ebd7ae0f508272bd2f8c398e5db04da7fa
pensivetraveler <pensivetraveler@gmail.com>
2025-05-16 18:36:42 +0900

    feat: uncamelize to snakeize

--- application/helpers/MY_inflector_helper.php
+++ application/helpers/MY_inflector_helper.php
@@ -1,41 +1,41 @@
 <?php
 // --------------------------------------------------------------------
 
-if ( ! function_exists('uncamelize'))
+if ( ! function_exists('snakeize'))
 {
-	/**
-	 * Uncamelize
-	 *
-	 * Reverse camelized string to underscore
-	 *
-	 * @param	string	$str	Input string
-	 * @return	string
-	 */
-	function uncamelize($str)
-	{
-		return strtolower(preg_replace('/[A-Z]/', '_$0', $str));
-	}
+    /**
+     * Snakeize
+     *
+     * Reverse camelized string to snakeized
+     *
+     * @param	string	$str	Input string
+     * @return	string
+     */
+    function snakeize($str)
+    {
+        return strtolower(preg_replace('/[A-Z]/', '_$0', $str));
+    }
 }
 
 // --------------------------------------------------------------------
 
 if ( ! function_exists('percentize'))
 {
-	/**
-	 * Percentize
-	 *
-	 * Takes number to percent
-	 *
-	 * @param	Number	$numerator	    Input Number
-	 * @param	Number	$denominator	Input Number
-	 * @return	string
-	 */
-	function percentize($numerator, $denominator = 100)
-	{
-		$fraction = (int)$numerator/(int)$denominator;
-		$formatter = new NumberFormatter('en_US', NumberFormatter::PERCENT);
-		return $formatter->format($fraction);
-	}
+    /**
+     * Percentize
+     *
+     * Takes number to percent
+     *
+     * @param	Number	$numerator	    Input Number
+     * @param	Number	$denominator	Input Number
+     * @return	string
+     */
+    function percentize($numerator, $denominator = 100)
+    {
+        $fraction = (int)$numerator/(int)$denominator;
+        $formatter = new NumberFormatter('en_US', NumberFormatter::PERCENT);
+        return $formatter->format($fraction);
+    }
 }
 
 
@@ -43,24 +43,24 @@ if ( ! function_exists('percentize'))
 
 if ( ! function_exists('get_language_code'))
 {
-	function get_language_code($language): string
-	{
-		$languages = [
-			'english' => 'en',
-			'korean' => 'ko',
-			'japanese' => 'ja',
-			'chinese' => 'zh',
-			'french' => 'fr',
-			'german' => 'de',
-			'spanish' => 'es',
-			'italian' => 'it',
-			'russian' => 'ru',
-			'portuguese' => 'pt',
-			'arabic' => 'ar',
-			'hindi' => 'hi'
-		];
+    function get_language_code($language): string
+    {
+        $languages = [
+            'english' => 'en',
+            'korean' => 'ko',
+            'japanese' => 'ja',
+            'chinese' => 'zh',
+            'french' => 'fr',
+            'german' => 'de',
+            'spanish' => 'es',
+            'italian' => 'it',
+            'russian' => 'ru',
+            'portuguese' => 'pt',
+            'arabic' => 'ar',
+            'hindi' => 'hi'
+        ];
 
-		$language = strtolower($language);
-		return $languages[$language] ?? 'en';
-	}
+        $language = strtolower($language);
+        return $languages[$language] ?? 'en';
+    }
 }
