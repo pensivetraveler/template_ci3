446340f38d199ab89be89d4e8f0a2b17c314648a
pensivetraveler <pensivetraveler@gmail.com>
2025-05-16 17:32:12 +0900

    feat: footer에 .env 파일 내 COMP_NAME 삽입

--- application/views/builder/includes/footer.php
+++ application/views/builder/includes/footer.php
@@ -6,19 +6,16 @@
 							<div class="footer-bottom py-5">
 								<div class="container d-flex flex-wrap justify-content-between flex-md-row flex-column text-center text-md-start">
 									<div class="mb-2 mb-md-0">
-										<span class="footer-text">©
+										<span class="footer-text"><?=APP_NAME?> ©
 										  <script>
 											document.write(new Date().getFullYear());
 										  </script>
-										  , Made with <i class="tf-icons ri-heart-fill text-danger"></i> by
+										  , Made by
 										</span>
-										<a href="https://pixinvent.com" target="_blank" class="footer-link fw-medium footer-theme-link">Pixinvent</a>
+										<a href="https://pixinvent.com" target="_blank" class="footer-link fw-medium footer-theme-link"><?=COMP_NAME?></a>
 									</div>
 									<div>
 										<a href="https://github.com/pixinvent" class="footer-link me-4" target="_blank"><i class="ri-github-fill"></i></a>
-										<a href="https://www.facebook.com/pixinvents/" class="footer-link me-4" target="_blank"><i class="ri-facebook-circle-fill"></i></a>
-										<a href="https://twitter.com/pixinvents" class="footer-link me-4" target="_blank"><i class="ri-twitter-fill"></i></a>
-										<a href="https://www.instagram.com/pixinvents/" class="footer-link" target="_blank"><i class="ri-instagram-line"></i></a>
 									</div>
 								</div>
 							</div>
