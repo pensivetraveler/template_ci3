4fc6fe2ae62a80d8fd41a732b6157a32fa6533d1
pensivetraveler <pensivetraveler@gmail.com>
2025-06-07 21:48:20 +0900

    Feat: base admin script added

---
+++ public/assets/admin/js/system_onload.js
@@ -0,0 +1,15 @@
+appPlugins.form.select2 = {
+    // 'class' : {
+    //     onHandler: {
+    //         change(e) {
+    //             if($('[name="class"]').val()) {
+    //                 // presetDynamicSelect2Options({
+    //                 //     node: $('[name="method"]'),
+    //                 //     url: common.API_URI +'/getMethodList',
+    //                 //     params: { class: $('[name="class"]').val() },
+    //                 // })
+    //             }
+    //         },
+    //     }
+    // },
+};
