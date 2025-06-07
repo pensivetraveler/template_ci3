4f7824a1a2b50784389265491446d1c47c035e05
pensivetraveler <pensivetraveler@gmail.com>
2025-06-07 19:51:07 +0900

    feat: option conf updated

--- application/config/extra/custom_options.php
+++ application/config/extra/custom_options.php
@@ -7,21 +7,35 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |--------------------------------------------------------------------------
 */
 $config['options'] = [
-	'default' => [
-		1 => 'Option 1',
-		2 => 'Option 2',
-	],
-	'yn' => [
+    'default' => [
+        1 => 'Option 1',
+        2 => 'Option 2',
+    ],
+    'bool' => [
+        1 => 'Y',
+        0 => 'N',
+    ],
+    'yn' => [
         'Y' => 'Y',
         'N' => 'N',
-	],
-	'gender' => [
-		'M' => '남',
-		'F' => '여',
-	],
+    ],
+    'gender' => [
+        'M' => '남',
+        'F' => '여',
+    ],
     'cfg_type' => [
         'text' => '텍스트',
         'password' => '비밀번호',
+    ],
+    'target' => [
+        '_self' => 'self',
+        '_blank' => 'blank',
+        '_popup' => 'popup',
+    ],
+    'menu_auth' => [
+        'title' => '메뉴명',
+        'class' => '클래스',
+        'method' => '메소드',
     ],
 ];
 
