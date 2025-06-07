79baae3ff33833dc0d29432623118957127d8bf6
pensivetraveler <pensivetraveler@gmail.com>
2025-06-07 20:22:44 +0900

    style: BASEPATH script edited

--- application/controllers/web/Common.php
+++ application/controllers/web/Common.php
@@ -1,4 +1,5 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Common extends MY_Builder_WEB
 {
