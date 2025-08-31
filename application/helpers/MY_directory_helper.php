6703ad709bb2703e979d3c4cee140af2ef773d74
pensivetraveler <pensivetraveler@gmail.com>
2025-08-31 19:27:20 +0900

    Revert "feat: real_path func 동작 시, document_root 적용"
    This reverts commit b74688091df53d586cf18adacdb94c46792dc1b7.

--- application/helpers/MY_directory_helper.php
+++ application/helpers/MY_directory_helper.php
@@ -3,8 +3,7 @@
 function make_directory($path, $mode = 0755): bool
 {
     $path_list = explode(DIRECTORY_SEPARATOR, $path);
-    $total_path = FCPATH;
-    if(substr($total_path,-1,1) === DIRECTORY_SEPARATOR) $total_path = substr($total_path, 0, strlen($total_path)-1);
+    $total_path = '';
     $result = true;
     for($i = 0; $i < count($path_list); $i++) {
         if(!$path_list[$i]) continue;
@@ -14,7 +13,7 @@ function make_directory($path, $mode = 0755): bool
             continue;
         }else{
             $result = @mkdir($total_path, DIR_READ_MODE);
-            @chmod($total_path, $mode);
+			@chmod($total_path, $mode);
             if(!$result) continue;
         }
     }
