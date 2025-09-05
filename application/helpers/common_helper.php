c66f30c08892ba29ac61c7b442f6dc271f825693
pensivetraveler <pensivetraveler@gmail.com>
2025-09-06 06:16:53 +0900

    feat: add guidV4

--- application/helpers/common_helper.php
+++ application/helpers/common_helper.php
@@ -213,3 +213,13 @@ if ( ! function_exists('get_starred_password'))
         return $res;
     }
 }
+
+if ( ! function_exists('guidV4'))
+{
+    function guidV4(): string {
+        $data = random_bytes(16);
+        $data[6] = chr((ord($data[6]) & 0x0f) | 0x40);
+        $data[8] = chr((ord($data[8]) & 0x3f) | 0x80);
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+}
