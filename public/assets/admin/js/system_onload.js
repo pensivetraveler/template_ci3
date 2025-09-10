ddc12210f154cdbf09f6b5ffa7248d47bdd4fe3e
pensivetraveler <pensivetraveler@gmail.com>
2025-09-11 07:40:05 +0900

    feat: 하위메뉴가  존재하는지 여부에 따라 reaonly 부여

--- public/assets/admin/js/system_onload.js
+++ public/assets/admin/js/system_onload.js
@@ -1,15 +1,39 @@
-appPlugins.form.select2 = {
-    // 'class' : {
-    //     onHandler: {
-    //         change(e) {
-    //             if($('[name="class"]').val()) {
-    //                 // presetDynamicSelect2Options({
-    //                 //     node: $('[name="method"]'),
-    //                 //     url: common.API_URI +'/getMethodList',
-    //                 //     params: { class: $('[name="class"]').val() },
-    //                 // })
-    //             }
-    //         },
-    //     }
-    // },
-};
+$(function () {
+    if(document.querySelector('#formRecord')) {
+        document.querySelector('#formRecord').addEventListener('applyFrmValues', function (e) {
+            if(Object.hasOwn(e.detail, 'record') && e.detail.record !== null) {
+                if(e.detail.record.class) {
+                    const value = e.detail.record.class.toLowerCase();
+                    const options = document.querySelectorAll("#form_page-class option");
+                    options.forEach(opt => {
+                        if (value === opt.value) {
+                            $('#form_page-class').val(opt.value).trigger('change');
+                        }
+                    });
+                }
+            }
+        });
+
+        document.querySelector('#formRecord').addEventListener('refreshPlugins', function (e) {
+            setSelect2Readonly('#form_page-class', true);
+            setSelect2Readonly('#form_page-method', true);
+
+            if(Object.hasOwn(e.detail, 'record') && e.detail.record !== null) {
+                const isSubMenu = parseInt(e.detail.record.is_sub_menu) === 1;
+                if(e.detail.record.class && !isSubMenu) {
+                    const url = getUrlWithIdentifiers(common.API_URI+'/options', {}, {
+                        class: e.detail.record.class
+                    });
+                    reloadSelect2('#form_page-method', url, [$('#form_page-class').val()]);
+                }else{
+                    if(isSubMenu) {
+                        setSelect2Readonly('#form_page-class');
+                        setSelect2Readonly('#form_page-method');
+                    }
+                    resetSelect2('#form_page-class', true);
+                    resetSelect2('#form_page-method', true);
+                }
+            }
+        });
+    }
+});
