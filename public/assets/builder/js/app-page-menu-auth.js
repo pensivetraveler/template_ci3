1876af94e8c5207d986a934b4debc7c4f13006bd
pensivetraveler <pensivetraveler@gmail.com>
2025-09-11 07:44:07 +0900

    feat: resetMenuAuthDB func defined

--- public/assets/builder/js/app-page-menu-auth.js
+++ public/assets/builder/js/app-page-menu-auth.js
@@ -1,3 +1,17 @@
 function afterChangeUserKind(node) {
     common.API_PARAMS.user_cd = node.value;
 }
+
+function resetMenuAuthDB() {
+    executeAjax({
+        url: common.API_URI + '/' + 'reset',
+        success: function(response) {
+            showAlert({
+                type: 'success',
+                title: 'Complete',
+                text: response.msg,
+                callback: reload,
+            });
+        },
+    });
+}
