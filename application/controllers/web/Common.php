3aa8d4a9c677940aa36c3928c53945888b0a0fdd
pensivetraveler <pensivetraveler@gmail.com>
2025-12-28 01:01:33 +0900

    feat:  add properties

--- application/controllers/web/Common.php
+++ application/controllers/web/Common.php
@@ -1,12 +1,14 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Common extends MY_Builder_WEB
+class Common extends MY_Controller_WEB
 {
-	function __construct()
-	{
-		$this->flag = 'web';
+    function __construct()
+    {
+        $this->flag = 'web';
+        $this->defaultController = 'home';
+        $this->baseViewPath = "web/layout/index";
 
-		parent::__construct();
-	}
+        parent::__construct();
+    }
 }
